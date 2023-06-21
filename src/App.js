920d5d6a770b52b156dfcbaf54a3691cb6760075
coriawork <manuelcoriawork@gmail.com>
2023-06-21 14:47:42 -0300

    cambios en la ruta del generos page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import HeaderComponent from './components/HeaderComponent';
 import FooterComponent from './components/FooterComponent';
 import NavBarComponent from './components/NavBarComponent';
 import './index.css'; // si lo saco se rompe el css por ahora..
-import GenerosPage from './pages/generos/GenerosPage';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Generos from './pages/generos/GenerosPage';
+import Plataformas from './pages/plataformas/Plataformas';
+import Dashboard from './pages/dashboard/Dashboard';
+
 
 function App() {
   
