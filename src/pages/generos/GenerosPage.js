33dbe2c7c80114342064c9262f7c0b2826d88f46
coriawork <manuelcoriawork@gmail.com>
2023-06-21 14:56:27 -0300

    GenerosPage.js creacion

---
+++ src/pages/generos/GenerosPage.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+
+import "./GenerosPage.css";
+import Table from "../components/Table";
+
+function GenerosPage() {
+  return (
+    <div>
+        <Table type='generos'></Table>
+    </div>
+    
+  );
+}
+
+export default GenerosPage;
