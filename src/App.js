e61b1a1195b35937827dc05d8d3ce91163221da8
facundolubo <facundolubo@gmail.com>
2023-06-22 08:41:20 -0300

    css

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React, { useState, useEffect } from 'react';
 import HeaderComponent from './components/HeaderComponent';
 import FooterComponent from './components/FooterComponent';
 import NavBarComponent from './components/NavBarComponent';
-import './index.css'; // si lo saco se rompe el css por ahora..
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Generos from './pages/generos/Generos';
 import Plataformas from './pages/plataformas/Plataformas';
 import Dashboard from './pages/dashboard/Dashboard';
+import './index.css'; // si lo saco se rompe el css por ahora..
 
 
 function App() {
