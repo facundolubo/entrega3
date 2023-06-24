cc218df28a2f4abdc297aaee3ad5bec9c2187595
coriawork <manuelcoriawork@gmail.com>
2023-06-24 17:41:17 -0300

    Mejoras y Arreglos de Table

---
+++ src/pages/plataformas/Plataformas.js
@@ -0,0 +1,10 @@
+import Table from "../components/Table";
+function Plataformas() {
+  return (
+    <div>
+      <Table type={'plataformas'}></Table>
+    </div>
+  );
+}
+
+export default Plataformas;
